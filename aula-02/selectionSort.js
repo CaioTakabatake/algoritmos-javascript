c002b5379131c22d45af2fff9e7dc7179f7c2b91
Caio <caio.soldera.taka@gmail.com>
2022-05-24 16:27:04 -0300

    selection sort para ordenar preços de livros

--- aula-02/selectionSort.js
+++ aula-02/selectionSort.js
@@ -1,6 +1,17 @@
 const livros = require('../aula-01/listaLivros');
-const monerValor = require('../aula-01/menorValor')
+const menorValor = require('../aula-01/menorValor')
 
-for (let atual = 0; atual < livros.length; atual++) {
-  
+for (let atual = 0; atual < livros.length - 1; atual++) {
+  let menor = menorValor(livros, atual);
+
+  let livroAtual = livros[atual];
+  console.log('posição atual', atual)
+  console.log('livro atual', livros[atual])
+  let livroMenor = livros[menor];
+  console.log('livro menor preço', livros[menor])
+
+  livros[atual] = livroMenor;
+  livros[menor] = livroAtual;
 }
+
+console.table(livros);
