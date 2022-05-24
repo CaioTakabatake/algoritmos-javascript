42a10f0c1245b9f35016407e6fb4d2d73dd841af
Caio <caio.soldera.taka@gmail.com>
2022-05-24 15:33:43 -0300

    transformando o algoritimo em uma função reutilizável

--- aula-01/menorValor.js
+++ aula-01/menorValor.js
@@ -3,8 +3,8 @@ const livros = require('./listaLivros');
 function menorValor(arrProdutos, posicaoInicial) {
   let maisBarato = posicaoInicial;
 
-  for (let atual = posicaoInicial; atual < livros.length; atual++) {
-    if (livros[atual].preco < livros[maisBarato].preco) {
+  for (let atual = posicaoInicial; atual < arrProdutos.length; atual++) {
+    if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco) {
       maisBarato = atual;
     }
   }
