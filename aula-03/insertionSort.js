c1866ec912af8bb48cde4b8844acfedad794048d
Caio <caio.soldera.taka@gmail.com>
2022-05-24 17:17:12 -0300

    ordenando de outra forma, com insertion sort

---
+++ aula-03/insertionSort.js
@@ -0,0 +1,21 @@
+const livros = require('../aula-01/listaLivros');
+
+function insertionSort(lista) {
+  for (let atual = 0; atual < lista.length; atual++) {
+    let analise = atual;
+
+    while (analise > 0 && lista[analise].preco < lista[analise - 1].preco) {
+      let itemAnalise = lista[analise];
+      let itemAnterior = lista[analise - 1];
+
+      lista[analise] = itemAnterior;
+      lista[analise - 1] = itemAnalise;
+
+      analise--
+    }
+  }
+
+  return lista;
+}
+
+console.log(insertionSort(livros));
