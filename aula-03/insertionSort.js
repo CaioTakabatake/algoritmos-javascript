d18249b7c457173c0444374bdbc158164a1aa95f
Caio <caio.soldera.taka@gmail.com>
2022-05-24 17:39:36 -0300

    ordenando de outra forma, com insertion sort

--- aula-03/insertionSort.js
+++ aula-03/insertionSort.js
@@ -4,7 +4,7 @@ function insertionSort(lista) {
   for (let atual = 0; atual < lista.length; atual++) {
     let analise = atual;
 
-    while (analise > 0 && lista[analise].preco < lista[analise - 1]) {
+    while (analise > 0 && lista[analise].preco < lista[analise - 1].preco) {
       let itemAnalise = lista[analise];
       let itemAnterior = lista[analise - 1];
 
