6dc7af4041af3b637bd1cd5f43ac3a108302b132
Caio <caio.soldera.taka@gmail.com>
2022-05-24 15:36:39 -0300

    criando selectionSort.js

--- .gitignore
+++
@@ -1,1 +0,0 @@
-selectionSort.js
---
+++ aula-02/selectionSort.js
@@ -0,0 +1,6 @@
+const livros = require('../aula-01/listaLivros');
+const monerValor = require('../aula-01/menorValor')
+
+for (let atual = 0; atual < livros.length; atual++) {
+  
+}
