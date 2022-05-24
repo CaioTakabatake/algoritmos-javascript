99b411cd3d129c092c467357ec208af591469079
Caio <caio.soldera.taka@gmail.com>
2022-05-24 15:30:40 -0300

    transformando o algoritimo em uma função reutilizável

--- aula-01/papelParaCodigo.js
+++ aula-01/papelParaCodigo.js
@@ -1,11 +1,18 @@
 const livros = require('./listaLivros');
 
-let maisBarato = 0;
+function menorValor(arrProdutos, posicaoInicial) {
+  let maisBarato = posicaoInicial;
 
-for (let atual = 0; atual < livros.length; atual++) {
-  if (livros[atual].preco < livros[maisBarato].preco) {
-    maisBarato = atual;
+  for (let atual = posicaoInicial; atual < livros.length; atual++) {
+    if (livros[atual].preco < livros[maisBarato].preco) {
+      maisBarato = atual;
+    }
   }
+
+  return maisBarato;
 }
 
+const maisBarato = menorValor(menorValor, 0);
 console.log(`Livro mais barato:\nNome: ${livros[maisBarato].titulo}\nPreço: ${livros[maisBarato].preco}`);
+
+module.exports = menorValor;
