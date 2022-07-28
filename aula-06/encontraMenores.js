4e6476d2d7557403c08a4dc1fd55367327483146
CaioTakabatake <74073758+CaioTakabatake@users.noreply.github.com>
2022-07-28 01:51:27 -0300

    organizando array atras do pivo

--- aula-06/encontraMenores.js
+++ aula-06/encontraMenores.js
@@ -12,7 +12,6 @@ function encontraMenores(pivo, array) {
     }
 
     trocaLugar(array, array.indexOf(pivo), menores);
-
     return array;
 }
 
@@ -24,4 +23,21 @@ function trocaLugar(array, de, para) {
     array[de] = element2;
 }
 
-console.log(encontraMenores(listaLivros[2], listaLivros));
+function divideNoPivo(array) {
+    let pivo = array[Math.floor(array.length / 2)];
+    encontraMenores(pivo, array);
+    let valoresMenores = 0;
+
+    for (let analisando = 0; analisando < array.length; analisando++) {
+        const atual = array[analisando];
+        if (atual.preco <= pivo.preco && atual !== pivo) {
+            trocaLugar(array, analisando, valoresMenores);
+            valoresMenores++
+        }
+    }
+
+    return array;
+}
+
+console.log(divideNoPivo(listaLivros))
+// console.log(encontraMenores(listaLivros[2], listaLivros));
